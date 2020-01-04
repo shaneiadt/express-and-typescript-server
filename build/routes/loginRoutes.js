ab9a0d49c77401465a15a695564d066bd7a210ac
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-01-04 11:57:06 +0000

    Create basic login form

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
-router.get('/', function (req, res) {
-    res.send("Hi there!");
+router.get('/login', function (req, res) {
+    res.send("\n    <div>\n      <form method=\"post\">\n        <div>\n          <label for=\"email\">Email</label>\n          <input name=\"email\" type=\"text\" />\n          </div>\n          <div>\n          <label for=\"password\">Password</label>\n          <input name=\"password\" type=\"password\" />\n        </div>\n        <button type=\"submit\">Submit</button>\n      </form>\n    </div>\n  ");
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,8 +2,22 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send(`Hi there!`);
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <form method="post">
+        <div>
+          <label for="email">Email</label>
+          <input name="email" type="text" />
+          </div>
+          <div>
+          <label for="password">Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  `);
 });
 
 export { router };
