8fbbf8dbae066859e62912568c68b4bb33c109e0
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-01-04 11:53:18 +0000

    Setup express router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi Bra!</h1>
-    </div>
-  `);
-});
+app.use(router);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('Listening on port 3000');
 });
